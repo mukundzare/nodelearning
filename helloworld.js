b821a64ab5f0fb0ea2f8a253485cf33ce4554cb2
Mukund Zare <mukund_zare@yahoo.com>
2015-07-19 21:12:09 +0200

    Express installed

--- helloworld.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-type' : 'text/html'});
-    res.end("Hello world \n");
-}).listen(1337,'127.0.0.1');
-console.log("Listening...");
